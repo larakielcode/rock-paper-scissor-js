f840f0f5dab54603f3e8503d1118ba9f1df1f7f1
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-29 16:15:16 +0800

    Completed the switch statements for all the choice in ea round

--- rps-game.js
+++ rps-game.js
@@ -4,7 +4,8 @@ function startGame() {
     const score = []; // store the score of the game
 
     let humanChoice;
-    let getComputerChoice
+    let getComputerChoice;
+    let playGame;
     const buttonsMenu = document.querySelector("#buttons");
     const resultContainer = document.querySelector("#results");
     const commentsPara = document.createElement("p");
@@ -22,16 +23,24 @@ function startGame() {
                 getComputerChoice = Entry.getComputerChoice();
                 console.log(`Your choice is rock`);
                 console.log(`Computer choice is`, getComputerChoice.choice);
-                let playGame = Entry.playRound(humanChoice.choice, getComputerChoice.choice);
+                playGame = Entry.playRound(humanChoice.choice, getComputerChoice.choice);
                 console.log(playGame);
                 break;
             case 'paper':
-                playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
-                checkWinner();
+                humanChoice = new Entry('paper', 'human');
+                getComputerChoice = Entry.getComputerChoice();
+                console.log(`Your choice is paper`);
+                console.log(`Computer choice is`, getComputerChoice.choice);
+                playGame = Entry.playRound(humanChoice.choice, getComputerChoice.choice);
+                console.log(playGame);
                 break;
             case 'scissor':
-                playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
-                checkWinner();
+                humanChoice = new Entry('scissor', 'human');
+                getComputerChoice = Entry.getComputerChoice();
+                console.log(`Your choice is scissors`);
+                console.log(`Computer choice is`, getComputerChoice.choice);
+                playGame = Entry.playRound(humanChoice.choice, getComputerChoice.choice);
+                console.log(playGame);
                 break;
         }
     });
@@ -135,96 +144,3 @@ class Entry {
 }
 
 startGame();
-
-
-/* 
-
-function resetScore() {
-    humanScore = 0;
-    computerScore = 0;
-}
-
-function checkWinner() {
-    if (humanScore === 5) {
-        resetScore();
-        commentsPara.textContent = "You win in a game of race to 5!"
-        commentsPara.style.color = "red";
-        resultContainer.appendChild(commentsPara);
-    }
-    if (computerScore === 5) {
-        resetScore();
-        commentsPara.textContent = "Computer wins in a game of race to 5!"
-        commentsPara.style.color = "red";
-        resultContainer.appendChild(commentsPara);
-    }
-    if (computerScore === 5 && humanScore === 5) {
-        resetScore();
-        commentsPara.textContent = "Its a draw match!"
-        commentsPara.style.color = "red";
-        resultContainer.appendChild(commentsPara);
-    }
-}
-
-function playRound(humanChoice, computerChoice) {
-    if ((computerChoice <= .333) && (humanChoice == "rock")) {
-        computerScore++;
-        humanScore++;
-        commentsPara.textContent = "Its a Draw! Both are rock."
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice <= .333) && (humanChoice == "paper")) {
-        humanScore++;
-        commentsPara.textContent = "You win! Paper beats rock.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice <= .333) && (humanChoice == "scissor")) {
-        computerScore++;
-        commentsPara.textContent = "You Lose! Rock beats scissor.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    }
-    if ((computerChoice >= .334 && computerChoice <= .666) && (humanChoice == "rock")) {
-        computerScore++;
-        commentsPara.textContent = "You Lose. Paper beats rock.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice >= .334 && computerChoice <= .666) && (humanChoice == "paper")) {
-        computerScore++;
-        humanScore++;
-        commentsPara.textContent = "Its a draw! Both are paper.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice >= .334 && computerChoice <= .666) && (humanChoice == "scissor")) {
-        humanScore++;
-        commentsPara.textContent = "You Win. Scissor beats paper.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    }
-    if ((computerChoice >= .667 && computerChoice <= 1) && (humanChoice == "rock")) {
-        humanScore++;
-        commentsPara.textContent = "You Win. Rock beats scissor.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice >= .667 && computerChoice <= 1) && (humanChoice == "paper")) {
-        computerScore++;
-        commentsPara.textContent = "You Lose. Scissor beats paper.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    } else if ((computerChoice >= .667 && computerChoice <= 1) && (humanChoice == "scissor")) {
-        computerScore++;
-        humanScore++;
-        commentsPara.textContent = "Its a draw! Both are scissor.";
-        resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-        resultContainer.appendChild(commentsPara);
-        resultContainer.appendChild(resultsPara);
-    }
-    return;
-} */
