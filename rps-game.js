28516fb59a7b3203649b0e02d439f9d6c4248c9c
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-29 16:12:26 +0800

    Refactor the whole codebase to use classes. Created a classes for the entry and added static functions for getting random choices and getting the winner in each round

--- rps-game.js
+++ rps-game.js
@@ -1,33 +1,143 @@
-// Define global vars
-let humanScore = 0;
-let computerScore = 0;
-
 // Define functions for choices
-let getComputerChoice = () => Math.random();
-const buttonsMenu = document.querySelector("#buttons");
-const resultContainer = document.querySelector("#results");
-const commentsPara = document.createElement("p");
-const resultsPara = document.createElement("p");
+function startGame() {
 
+    const score = []; // store the score of the game
 
-buttonsMenu.addEventListener("click", (event) => {
-    let target = event.target;
+    let humanChoice;
+    let getComputerChoice
+    const buttonsMenu = document.querySelector("#buttons");
+    const resultContainer = document.querySelector("#results");
+    const commentsPara = document.createElement("p");
+    const resultsPara = document.createElement("p");
 
-    switch (target.id) {
-        case 'rock':
-            playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
-            checkWinner();
-            break;
-        case 'paper':
-            playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
-            checkWinner();
-            break;
-        case 'scissor':
-            playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
-            checkWinner();
-            break;
+    buttonsMenu.addEventListener("click", (event) => {
+        let target = event.target;
+
+        resultContainer.appendChild(commentsPara);
+        resultContainer.appendChild(resultsPara);
+
+        switch (target.id) {
+            case 'rock':
+                humanChoice = new Entry('rock', 'human');
+                getComputerChoice = Entry.getComputerChoice();
+                console.log(`Your choice is rock`);
+                console.log(`Computer choice is`, getComputerChoice.choice);
+                let playGame = Entry.playRound(humanChoice.choice, getComputerChoice.choice);
+                console.log(playGame);
+                break;
+            case 'paper':
+                playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
+                checkWinner();
+                break;
+            case 'scissor':
+                playRound(target.id, parseFloat(getComputerChoice().toFixed(3)));
+                checkWinner();
+                break;
+        }
+    });
+
+} // end startGame();
+
+class Entry {
+
+    constructor(choice, playerName) {
+        this.choice = choice;
+        this.player = playerName;
     }
-});
+
+    static randomChoice(arr) {
+        const choiceIndex = Math.floor(Math.random() * arr.length);
+        return arr[choiceIndex];
+    }
+
+    static getComputerChoice() {
+        const choices = [
+            'rock',
+            'paper',
+            'scissor',
+        ];
+
+        const computerChoice = this.randomChoice(choices);
+        return new Entry(computerChoice, 'computer');
+    }
+
+    static playRound(humanChoice, computerChoice) {
+        if ((computerChoice == "rock") && (humanChoice == "rock")) {
+            return `both;It's a Draw! Both are ROCK!`;
+            /* computerScore++;
+            humanScore++; */
+            /* commentsPara.textContent = "Its a Draw! Both are rock."
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "rock" && humanChoice == "paper") {
+            return `human;You Win! PAPER beats ROCK`;
+            /* humanScore++;
+            commentsPara.textContent = "You win! Paper beats rock.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "rock" && humanChoice == "scissor") {
+            return `computer;You Lose! ROCK beats SCISSORS`;
+            /* computerScore++;
+            commentsPara.textContent = "You Lose! Rock beats scissor.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        }
+        if (computerChoice == "paper" && humanChoice == "rock") {
+            return `computer;You Lose! PAPER beats ROCK`;
+            /* computerScore++;
+            commentsPara.textContent = "You Lose. Paper beats rock.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "paper" && humanChoice == "paper") {
+            return `BOTH;It's a DRAW! Both are PAPER`;
+            /* computerScore++;
+            humanScore++;
+            commentsPara.textContent = "Its a draw! Both are paper.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "paper" && humanChoice == "scissor") {
+            return `human;You Win! SCISSORS beats PAPER`;
+            /* humanScore++;
+            commentsPara.textContent = "You Win. Scissor beats paper.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        }
+        if (computerChoice == "scissor" && humanChoice == "rock") {
+            return `human;You Win! ROCK beats SCISSORS`;
+            /* humanScore++;
+            commentsPara.textContent = "You Win. Rock beats scissor.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "scissor" && humanChoice == "paper") {
+            return `computer;You Lose! SCISSORS beats PAPER`;
+            /* computerScore++;
+            commentsPara.textContent = "You Lose. Scissor beats paper.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        } else if (computerChoice == "scissor" && humanChoice == "scissor") {
+            return `BOTH;It's a DRAW! Both are SCISSORS`;
+            /* computerScore++;
+            humanScore++;
+            commentsPara.textContent = "Its a draw! Both are scissor.";
+            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
+            resultContainer.appendChild(commentsPara);
+            resultContainer.appendChild(resultsPara); */
+        }
+    }
+}
+
+startGame();
+
+
+/* 
 
 function resetScore() {
     humanScore = 0;
@@ -117,4 +227,4 @@ function playRound(humanChoice, computerChoice) {
         resultContainer.appendChild(resultsPara);
     }
     return;
-}
+} */
