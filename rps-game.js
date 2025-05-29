8919658e8eb987d7f33927571b1ffee0ce4a4c3e
Larakielcode <4220069+larakielcode@users.noreply.github.com>
2025-05-29 17:27:37 +0800

    removed all the unneccessary commented lines

--- rps-game.js
+++ rps-game.js
@@ -150,73 +150,43 @@ class Entry {
 
     static playRound(humanChoice, computerChoice) {
         if ((computerChoice == "rock") && (humanChoice == "rock")) {
+
             return `both;It's a Draw! Both are ROCK!`;
-            /* computerScore++;
-            humanScore++; */
-            /* commentsPara.textContent = "Its a Draw! Both are rock."
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "rock" && humanChoice == "paper") {
+
             return `human;You Win! PAPER beats ROCK`;
-            /* humanScore++;
-            commentsPara.textContent = "You win! Paper beats rock.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "rock" && humanChoice == "scissor") {
+
             return `computer;You Lose! ROCK beats SCISSORS`;
-            /* computerScore++;
-            commentsPara.textContent = "You Lose! Rock beats scissor.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         }
         if (computerChoice == "paper" && humanChoice == "rock") {
+
             return `computer;You Lose! PAPER beats ROCK`;
-            /* computerScore++;
-            commentsPara.textContent = "You Lose. Paper beats rock.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "paper" && humanChoice == "paper") {
+
             return `both;It's a DRAW! Both are PAPER`;
-            /* computerScore++;
-            humanScore++;
-            commentsPara.textContent = "Its a draw! Both are paper.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "paper" && humanChoice == "scissor") {
+
             return `human;You Win! SCISSORS beats PAPER`;
-            /* humanScore++;
-            commentsPara.textContent = "You Win. Scissor beats paper.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         }
         if (computerChoice == "scissor" && humanChoice == "rock") {
+
             return `human;You Win! ROCK beats SCISSORS`;
-            /* humanScore++;
-            commentsPara.textContent = "You Win. Rock beats scissor.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "scissor" && humanChoice == "paper") {
+
             return `computer;You Lose! SCISSORS beats PAPER`;
-            /* computerScore++;
-            commentsPara.textContent = "You Lose. Scissor beats paper.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         } else if (computerChoice == "scissor" && humanChoice == "scissor") {
+
             return `both;It's a DRAW! Both are SCISSORS`;
-            /* computerScore++;
-            humanScore++;
-            commentsPara.textContent = "Its a draw! Both are scissor.";
-            resultsPara.textContent = "Computer Score : " + computerScore + " Your Score : " + humanScore;
-            resultContainer.appendChild(commentsPara);
-            resultContainer.appendChild(resultsPara); */
+
         }
     }
 }
